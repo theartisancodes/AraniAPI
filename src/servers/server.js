e63131c2c37500d7e2586723949c1e09a1390bac
AnguleMathias <angulemathias3@gmail.com>
2019-08-19 16:21:29 +0300

    Setup jest

--- src/servers/server.js
+++ src/servers/server.js
@@ -1,8 +1,8 @@
-import { ApolloServer } from 'apollo-server-express';
-import { schema, resolvers } from 'graphql/index'
+import { ApolloServer } from "apollo-server-express";
+import { schema, resolvers } from "graphql/index";
 
 const Server = (schema, resolvers) => {
-   return new ApolloServer({ typeDefs: schema, resolvers })
+  return new ApolloServer({ typeDefs: schema, resolvers });
 };
 
 module.exports = {
